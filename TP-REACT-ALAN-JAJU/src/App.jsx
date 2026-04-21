5300fc4b810b06d1b92f302036c54a2da0aaef19
NICOLÁS JAIR JAJURIN <48861075@ort>
2026-04-21 09:47:40 -0300

    Usando localstorage,getitem,set,item

--- TP-REACT-ALAN-JAJU/src/App.jsx
+++ TP-REACT-ALAN-JAJU/src/App.jsx
@@ -1,15 +1,22 @@
 import Formulario from './components/Formulario/Formulario'
 import Listado from './components/Listado/Listado'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 function App() {
 
-  const [citas,setCita]= useState([{mascota:"Hernesto", propietario:"Juan", fecha:"2023-10-10", hora:"10:00", sintomas:"Dolor de estómago" },
-    {mascota:"Javier", propietario:"Miguel", fecha:"2023-10-10", hora:"10:00", sintomas:"Dolor de pata" }, 
-    {mascota:"Damian", propietario:"Cymerman", fecha:"2023-10-10", hora:"10:00", sintomas:"fiebre" }
+  const resultado = JSON.parse(localStorage.getItem("citas"))
 
-  ])
+  const [citas, setCita] = useState(resultado)
+
+  useEffect(() => {
+    
+    localStorage.setItem( "citas", JSON.stringify(citas))
+
+    
+  }, [citas])
+
+
   return (
     <>
       <h1>ADMINISTRADOR DE PACIENTES</h1>
